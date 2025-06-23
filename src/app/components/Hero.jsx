84757ddeba82f4be47f8b17f14fc17e7cb8eb665
choity <afsanamimi194@gmail.com>
2025-06-23 22:29:01 +0600

    project created

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <div>
+            <div
+                className="hero absolute h-100 top-0 left-0 pl-0 bg-gradient-to-r from-[#151520] to-[rgba(21, 21, 21, 0.00)] "
+                style={{
+                    backgroundImage:
+                        "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                }}
+            >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
